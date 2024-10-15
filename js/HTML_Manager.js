0e8b067d8e10afd8e28c24fded28d9e14582b660
Bob Englert <166454873+dbe-inc@users.noreply.github.com>
2024-10-15 10:17:31 -0600

    Oct 14 2024  Changes to be committed:	modified:   education.html	modified:   js/HTML_Manager.js  modified: HTML_Manager

--- js/HTML_Manager.js
+++ js/HTML_Manager.js
@@ -1,31 +1,31 @@
 let HTML_Manager = {
     //Properties
-  //  loaded: false,
-    liveDropdown: "Packets",
-    eduContentsSelected: "none",
-    htmlTargetEle: {},
-    eduPacketsDropdown: {},
+    //  loaded: false,
+    eduManagersDropdown: {},
     eduResourcesDropdown: {},
     eduTestingDropdown: {},
     eduTrainingDropdown: {},
-    packetsLabel: {},
+    managersLabel: {},
     resourcesLabel: {},
     trainingLabel: {},
     testingLabel: {},
     proposalLabel: {},
+    activeDropdown:{},
+    activeSelectorLabel: {},
+    eduContentsSelected: "edu-managers-dropdown",
+    htmlTargetEle: {},
 
     //Methods
-    load: function(HTMLsourceFile) {
-        //alert("in load");
-      //  alert("HTMLsourceFile: " + HTMLsourceFile);
-        if(HTMLsourceFile == "html/Proposal.html"){
-            this.setActiveInactiveLabel("set", "Proposal");
-            this.setActiveInactiveLabel("reSet", this.liveDropdown);
-        }
-        this.htmlTargetEle.style.zIndex = 10;
-        this.htmlTargetEle.style.opacity= "1";
-      //  if(!this.loaded){
-            fetch(HTMLsourceFile)
+    load: function (HTMLsourceFile) {
+       // alert("in load");
+        //  alert("HTMLsourceFile: " + HTMLsourceFile);
+       /* if (HTMLsourceFile == "html/Proposal.html") {
+            this.setActiveInactiveLabel(this.eduManagersDropdown);
+        }*/
+       // this.htmlTargetEle.style.zIndex = 10;
+       // this.htmlTargetEle.style.opacity = "1";
+        //  if(!this.loaded){ 
+        fetch(HTMLsourceFile)
             .then(res => {
                 //alert("in res =>");
                 if (res.ok) {
@@ -37,144 +37,67 @@ let HTML_Manager = {
                 //alert("loading resultHTML");
                 this.htmlTargetEle.innerHTML = resultHTML;
             })
-
-           // this.loaded = true;
-     //   } 
-      //  this.showing = true; 
-    },
-
-    display: function(contentsSelected){
-        switch(this.liveDropdown){
-            case "Packets":
-              //  alert("in Packets & " + this.liveDropdown + " Selected");
-                this.eduPacketsDropdown.style.display = "none";
-                this.packetsLabel.classList.remove("edu-label-active"); 
-                break;
-            case "Resources":
-             //   alert("in Resources & " + this.liveDropdown + " Selected");
-                this.eduResourcesDropdown.style.display = "none";
-                this.resourcesLabel.classList.remove("edu-label-active"); 
-                break;
-            case "Training":
-             //   alert("in Training & " + this.liveDropdown + " Selected");
-                this.eduTrainingDropdown.style.display = "none";
-                this.trainingLabel.classList.remove("edu-label-active"); 
-                break;
-            case "Testing":
-             //   alert("in Testing & " + this.liveDropdown + " Selected");
-                this.eduTestingDropdown.style.display = "none";
-                this.testingLabel.classList.remove("edu-label-active"); 
-                break;
-            default: alert("no such edu-contents");
-        }
-        switch(contentsSelected){
-            case "Packets":
-                if( window.getComputedStyle( this.eduPacketsDropdown ).display === "none" ){
-                    this.eduPacketsDropdown.style.display = "flex";
-                  //  alert("add orangered color to Packets");
-                    this.setActiveInactiveLabel("set", "Packets");
-                  //  this.packetsLabel.classList.add("edu-label-active");
-                }
-                this.liveDropdown = "Packets";
-                break;
-            case "Resources":
-                if( window.getComputedStyle( this.eduResourcesDropdown ).display === "none" ){
-                    this.eduResourcesDropdown.style.display = "flex";
-                    this.setActiveInactiveLabel("set", "Resources");
-                   // this.resourcesLabel.classList.add("edu-label-active");
-                }
-                this.liveDropdown = "Resources";
-                break;
-            case "Training":
-                if( window.getComputedStyle( this.eduTrainingDropdown ).display === "none" ){
-                    this.eduTrainingDropdown.style.display = "flex";
-                    this.setActiveInactiveLabel("set", "Training");
-                  //  this.trainingLabel.classList.add("edu-label-active");
-                }
-                this.liveDropdown = "Training";
-                break;
-            case "Testing":
-                if( window.getComputedStyle( this.eduTestingDropdown ).display === "none" ){
-                    this.eduTestingDropdown.style.display = "flex";
-                    this.setActiveInactiveLabel("set", "Testing");
-                   // this.testingLabel.classList.add("edu-label-active");
-                }
-                this.liveDropdown = "Testing";
-                break;
-            default: alert("no such edu-contents");
-        }
-        this.showingDropdown = contentsSelected;
     }, 
 
-    setActiveInactiveLabel: function(setReset, label){
-        switch(label){
-            case "Proposal":
-                if(setReset == "set"){
-                    this.proposalLabel.classList.add("edu-label-active");
-                }else{
-                    this.proposalLabel.classList.remove("edu-label-active");
-                }
-                break;
-            case "Packets":
-                if(setReset == "set"){
-                    this.packetsLabel.classList.add("edu-label-active");
-                }else{
-                    this.packetsLabel.classList.remove("edu-label-active");
-                }
+    display: function (contentsSelected) {
+       // alert("in display");
+        //alert("contentsSelected: " + contentsSelected);
+        this.activeDropdown.classList.replace("edu-dropdown-display","edu-dropdown-none")
+        this.activeDropdown.style.display = "none";
+        this.activeSelectorLabel.classList.remove("edu-label-active");
+        switch (contentsSelected) {
+            case "Managers":
+                this.activeDropdown = this.eduManagersDropdown;
+                this.activeSelectorLabel = this.managersLabel;
                 break;
             case "Resources":
-                if(setReset == "set"){
-                    this.resourcesLabel.classList.add("edu-label-active");
-                }else{
-                    this.resourcesLabel.classList.remove("edu-label-active");
-                }
+                this.activeDropdown = this.eduResourcesDropdown;
+                this.activeSelectorLabel = this.resourcesLabel;
                 break;
             case "Training":
-                if(setReset == "set"){
-                    this.trainingLabel.classList.add("edu-label-active");
-                }else{
-                    this.trainingLabel.classList.remove("edu-label-active");
-                }
+                this.activeDropdown = this.eduTrainingDropdown;
+                this.activeSelectorLabel = this.trainingLabel;
                 break;
             case "Testing":
-                if(setReset == "set"){
-                    this.testingLabel.classList.add("edu-label-active");
-                }else{
-                    this.testingLabel.classList.remove("edu-label-active");
-                }
+                this.activeDropdown = this.eduTestingDropdown;
+                this.activeSelectorLabel = this.testingLabel;
                 break;
             default: alert("no such edu-contents");
         }
+        this.activeDropdown.classList.replace("edu-dropdown-none","edu-dropdown-replace")
+        this.activeDropdown.style.display = "flex";
+        this.activeSelectorLabel.classList.add("edu-label-active"); 
     },
 
-    close: function(){
+    close: function() {
         this.htmlTargetEle.style.opacity = "0";
         this.setActiveInactiveLabel("set", this.liveDropdown);
         this.setActiveInactiveLabel("reSet", "Proposal");
-       /* this.htmlTargetEle.style.zIndex = -10; */
     },
 
-    transitionHasEnded: function(){
+    transitionHasEnded: function() {
         //alert("in transitionHasEnded");
         let newEle = document.getElementById("html-target-id");
-        if(newEle.style.opacity == 0){
-            newEle.style.zIndex = -10;	
+        if (newEle.style.opacity == 0) {
+            newEle.style.zIndex = -10;
         }
     },
 
-    init: function(){
-        //alert("in HTML_Manager init()");
+    init: function() {
+       // alert("in HTML_Manager init()");
         this.htmlTargetEle = document.getElementById("html-target-id");
-        this.htmlTargetEle.addEventListener("transitionend",this.transitionHasEnded);
-        this.eduPacketsDropdown = document.getElementById("edu-packets-dropdown-id");
+        this.htmlTargetEle.addEventListener("transitionend", this.transitionHasEnded);
+        this.eduManagersDropdown = document.getElementById("edu-managers-dropdown-id");
         this.eduResourcesDropdown = document.getElementById("edu-resources-dropdown-id");
         this.eduTrainingDropdown = document.getElementById("edu-training-dropdown-id");
         this.eduTestingDropdown = document.getElementById("edu-testing-dropdown-id");
-        this.packetsLabel = document.getElementById("packets-label-id"); 
-        this.resourcesLabel = document.getElementById("resources-label-id"); 
-        this.trainingLabel = document.getElementById("training-label-id"); 
-        this.testingLabel = document.getElementById("testing-label-id"); 
+        this.managersLabel = document.getElementById("managers-label-id");
+        this.resourcesLabel = document.getElementById("resources-label-id");
+        this.trainingLabel = document.getElementById("training-label-id");
+        this.testingLabel = document.getElementById("testing-label-id");
         this.proposalLabel = document.getElementById("proposal-label-id");
+        this.activeDropdown = this.eduManagersDropdown;
+        this.activeSelectorLabel = this.managersLabel;
         //alert("after HTML_Manager init()");
     }
 }
