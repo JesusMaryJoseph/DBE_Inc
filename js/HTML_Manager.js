25257ef0fb7843529b106744c8247e6637b91afd
Bob Englert <166454873+dbe-inc@users.noreply.github.com>
2024-11-15 11:20:12 -0700

    Nov 15 2024 	new file:   css/ceo/ceo.css 	modified:   css/education/edu.css 	modified:   education.html 	modified:   html/Proposal.html 	modified:   js/HTML_Manager.js

--- js/HTML_Manager.js
+++ js/HTML_Manager.js
@@ -7,7 +7,6 @@
 
 
 /* EduHTML_Manager */
-
 let EduHTML_Manager = {
     //Properties
     //  loaded: false,
@@ -43,8 +42,10 @@ let EduHTML_Manager = {
                 alert("no such load target");
         }
        // alert("this.htmlTargetEle.style.opacity = " + this.eduEle.style.opacity);
-         this.htmlTargetEle.style.zIndex = 10;
+        this.htmlTargetEle.style.zIndex = 10;
+        //alert("this.htmlTargetEle.style.zIndex =  " + this.htmlTargetEle.style.zIndex);
         this.htmlTargetEle.style.opacity = "1";
+        //alert("this.htmlTargetEle.style.opacity =  " + this.htmlTargetEle.style.opacity);
         //  if(!this.loaded){ 
         fetch(HTMLsourceFile)
             .then(res => {
@@ -97,8 +98,10 @@ let EduHTML_Manager = {
         this.activeSelectorLabel.classList.add("edu-label-active"); 
     },
 
-    close: function(selectedNav) {
-        this.htmlTargetEle.style.zIndex = -10;
+    close: function() { //selectedNav
+       // alert("this.htmlTargetEle.style.zIndex =  " + this.htmlTargetEle.style.zIndex);
+        //alert("this.htmlTargetEle.style.opacity =  " + this.htmlTargetEle.style.opacity);
+      //  this.htmlTargetEle.style.zIndex = -10;
         this.htmlTargetEle.style.opacity = "0";
       /*  if(!EduNavHandler.hidden){
             EduNavHandler.showHide(selectedNav);
@@ -108,36 +111,101 @@ let EduHTML_Manager = {
     },
 
     transitionHasEnded: function() {
-       // alert("in transitionHasEnded");
+        //alert("in transitionHasEnded");
+       // alert("this.htmlTargetEle.style.opacity =  " + 'this.htmlTargetEle.style.opacity');
         if (this.htmlTargetEle.style.opacity == 0) {
+            //alert("this.htmlTargetEle.style.opacity =  " + 'this.htmlTargetEle.style.opacity');
             this.htmlTargetEle.style.zIndex = -10;
         }
        /* if(!EduNavHandler.hidden){
-                EduNavHandler.showHide(selectedNav);
+        EduNavHandler.showHide(selectedNav);
         } */
     }, 
 
-    init: function() {
-    // alert("in EduHTML_Manager init()");
-        this.eduEle = document.getElementById("edu-contents-id");
-        this.eduEle.addEventListener("transitionend", this.transitionHasEnded);
-        this.eduArticleEle = document.getElementById("edu-article-id");
-        this.eduArticleEle.addEventListener("transitionend",this.transitionHasEnded);
-        this.eduManagersSelector = document.getElementById("edu-managers-selector-id");
-        this.eduResourcesSelector = document.getElementById("edu-resources-selector-id");
-        this.eduTrainingSelector = document.getElementById("edu-training-selector-id");
-        this.eduTestingSelector = document.getElementById("edu-testing-selector-id");
-        this.managersLabel = document.getElementById("managers-label-id");
-        this.resourcesLabel = document.getElementById("resources-label-id");
-        this.trainingLabel = document.getElementById("training-label-id");
-        this.testingLabel = document.getElementById("testing-label-id");
-        this.activeSelector = this.eduManagersSelector;
-        this.activeSelectorLabel = this.managersLabel;
+    init: function(caller) {
+     //alert("in EduHTML_Manager init()");
+        if(caller == "CEO"){
+            //alert("caller =  " + caller);
+            this.eduEle = document.getElementById("ceo-contents-id");
+            this.eduEle.addEventListener("transitionend", this.transitionHasEnded);
+        }else{
+            this.eduEle = document.getElementById("edu-contents-id");
+            this.eduEle.addEventListener("transitionend", this.transitionHasEnded);
+            this.eduArticleEle = document.getElementById("edu-article-id");
+            this.eduArticleEle.addEventListener("transitionend",this.transitionHasEnded);
+            this.eduManagersSelector = document.getElementById("edu-managers-selector-id");
+            this.eduResourcesSelector = document.getElementById("edu-resources-selector-id");
+            this.eduTrainingSelector = document.getElementById("edu-training-selector-id");
+            this.eduTestingSelector = document.getElementById("edu-testing-selector-id");
+            this.managersLabel = document.getElementById("managers-label-id");
+            this.resourcesLabel = document.getElementById("resources-label-id");
+            this.trainingLabel = document.getElementById("training-label-id");
+            this.testingLabel = document.getElementById("testing-label-id");
+            this.activeSelector = this.eduManagersSelector;
+            this.activeSelectorLabel = this.managersLabel;
+        }
      // alert("leaving EduHTML_Manager init()");
     }
 }
 /* End EduHTML_Manager */
 
+
+
+function transitionHasEnded(){
+    //alert("in transitionhasEnded");
+    CeoHTML_Manager.finishTransition();
+}
+
+/* CeoHTML_Manager */
+let CeoHTML_Manager = {
+    //Properties
+    ceoEle: {},
+
+    //Methods
+    load: function(HTMLsourceFile) {
+        //alert("in load");
+        this.ceoEle.style.zIndex = 10;
+    // alert("this.ceoEle.style.zIndex =  " + this.ceoEle.style.zIndex);
+        this.ceoEle.style.opacity = 1;
+        fetch(HTMLsourceFile)
+            .then(res => {
+                //alert("in res =>");
+                if (res.ok) {
+                    //alert("res.ok");
+                    return res.text();
+                }
+            })
+            .then(resultHTML => {
+                //alert("loading resultHTML");
+                this.ceoEle.innerHTML = resultHTML;
+            })
+    },
+
+    close: function() { 
+        //alert("this.ceoEle.style.zIndex =  " + this.ceoEle.style.zIndex);
+        //alert("this.ceo.style.opacity =  " + this.ceoEle.style.opacity);
+        this.ceoEle.style.opacity = 0;
+        //this.opacity = 0;
+    },
+
+    finishTransition: function() {
+        //alert("in finish.Transition");
+       // alert("this.ceoEle.style.opacity =  " + this.ceoEle.style.opacity);
+        if (this.ceoEle.style.opacity == 0) {
+            this.ceoEle.style.zIndex = -10;
+        }else{
+            this.ceoEle.style.zIndex = 10;
+        }
+    },
+
+    init: function() {
+    //alert("in CeoHTML_Manager init()");
+        this.ceoEle = document.getElementById("ceo-contents-id");
+        this.ceoEle.addEventListener("transitionend", transitionHasEnded, false);
+    //alert("leaving EduHTML_Manager init()");
+    }
+}
+/* End CeoHTML_Manager */	
 
 /* Begin TableManager  */
 let TableManager = {
