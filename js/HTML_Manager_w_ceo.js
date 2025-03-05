c769713e720774fd648d901d4a36e41d2f32da5c
Bob Englert <166454873+dbe-inc@users.noreply.github.com>
2025-03-05 11:44:12 -0700

    March 5 2025

--- js/HTML_Manager_w_ceo.js
+++ js/HTML_Manager_w_ceo.js
@@ -574,8 +574,11 @@ let PracticeTruthTableManager = {
 /* EduHTML_Manager */
     let EduHTML_Manager = {
         //Properties
+      /*  eduNavOneContentsEle: {},
+        eduNavTwoContentsEle: {},*/
         eduContentsEle: {},
         icDataEle: {},
+      /*  targetEle: {},*/
         requestedFile: "",
     
         //Methods
@@ -585,6 +588,33 @@ let PracticeTruthTableManager = {
         // alert("targetEdu = " + targetEdu);
         // alert("requestingEle = " + requestingEle);
             this.requestedFile = HTMLsourceFile;
+         /*   switch(targetEdu){
+                case "nav-one":
+                   // alert("in case nav-one");
+                    this.targetEle = this.eduNavOneContentsEle;
+                break;
+                case "nav-two":
+                    //alert("in case nav-two");
+                    this.targetEle = this.eduNavTwoContentsEle;
+                break;
+                case "edu-contents":
+                   // alert("in case contents");
+                    this.targetEle = this.eduContentsEle;
+                    //alert("this.eduContentsEle.classList = " + this.eduContentsEle.classList);
+                break;
+                default: alert("No Such Target");
+            }*/
+           /* if(requestingEle ==="videos" || requestingEle === "datasheets" || requestingEle === "packets"){
+                // alert("requestingEle = " + requestingEle);
+                // alert("requesting Element was 'true'");
+                this.eduNavOneContentsEle.innerHTML = "";
+                this.eduNavTwoContentsEle.innerHTML = "";
+            }*/
+           // alert("just before  this.targetEle.classList.replace");
+            //this.targetEle.classList.replace("transition-0-1","transition-1-0");
+           // alert("between");
+            //this.targetEle.style.opacity = 1;
+           // alert("this.load() next");
             this.load(contentId);
         },
 
@@ -592,6 +622,7 @@ let PracticeTruthTableManager = {
             //alert("in load");
             //alert("HTMLsourceFile: " + HTMLsourceFile);
             //alert("targetOption =  " + targetOption);
+
             fetch(this.requestedFile)
                 .then(res => {
                    // alert("in res =>");
@@ -622,31 +653,96 @@ let PracticeTruthTableManager = {
         },
 
         close: function(){
-           // alert("in EduHTML_Manager this.close");
             this.icDataEle.style.opacity = 0;
             this.icDataEle.style.display = "none";
         },
 
         init: function() {
         //alert("in EduHTML_Manager init()");
+           /* this.eduNavOneContentsEle = document.getElementById("edu-nav-one-contents-id");
+           this.eduNavOneContentsEle.addEventListener("transitionend", eduTransitionHasEnded, false);
+            this.eduNavTwoContentsEle = document.getElementById("edu-nav-two-contents-id");
+            this.eduNavTwoContentsEle.addEventListener("transitionend", eduTransitionHasEnded, false);
+            this.eduNavOneContentsEle.style.opacity = 1;
+            this.eduNavTwoContentsEle.style.opacity = 1;*/
          //alert("this.eduNavOneContentsEle.classList = " + this.eduNavOneContentsEle.classList);
             this.eduContentsEle = document.getElementById("edu-contents-id");
         // alert("this.eduContentsEle.classList = " + this.eduContentsEle.classList);
             this.eduContentsEle.addEventListener("transitionend", this.show().bind, false);
             this.eduContentsEle.style.opacity = 1;   /*eduTransitionHasEnded*/
         //    alert("end of EduHTML_Manager init()");
-            this.icDataEle = document.getElementById("ic-data-contents-id");
+            this.icDataEle = document.getElementById("ic-data-id");
         }
     }
     /* End EduHTML_Manager */
 
+
+/*
+function ceoTransitionHasEnded(){
+    //alert("in transitionhasEnded");
+    CeoHTML_Manager.ceoFinishTransition();
+} */
+
+/* CeoHTML_Manager */
+    let CeoHTML_Manager = {
+        //Properties
+        ceoEle: {},
+
+        //Methods
+        load: function(HTMLsourceFile) {
+           //alert("in load HTMLsourceFile =  " + HTMLsourceFile);
+            this.ceoEle.innerHTML = "";
+            this.ceoEle.style.zIndex = 10;
+        // alert("this.ceoEle.style.zIndex =  " + this.ceoEle.style.zIndex);
+            this.ceoEle.style.opacity = 1;
+            this.ceoEle.style.display = "block";
+            fetch(HTMLsourceFile)
+                .then(res => {
+                   // alert("in res =>");
+                    if (res.ok) {
+                        //alert("res.ok");
+                        return res.text();
+                    }
+                })
+                .then(resultHTML => {
+                   // alert("loading resultHTML");
+                    this.ceoEle.innerHTML = resultHTML;
+                })
+        },
+
+        close: function() { 
+            //alert("this.ceoEle.style.zIndex =  " + this.ceoEle.style.zIndex);
+            //alert("this.ceo.style.opacity =  " + this.ceoEle.style.opacity);
+            this.ceoEle.style.opacity = 0;
+            this.ceoEle.style.display = "none";
+            //this.opacity = 0;
+        },
+
+        ceoFinishTransition: function() {
+            //alert("in finish.Transition");
+        // alert("this.ceoEle.style.opacity =  " + this.ceoEle.style.opacity);
+            if (this.ceoEle.style.opacity == 0) {
+                this.ceoEle.style.zIndex = -10;
+            }else{
+                this.ceoEle.style.zIndex = 10;
+            }
+        },
+
+        init: function() {
+        //alert("in CeoHTML_Manager init()");
+            this.ceoEle = document.getElementById("ceo-contents-id");
+            this.ceoEle.addEventListener("transitionend", this.ceoFinishTransition().bind, false);
+        //alert("leaving EduHTML_Manager init()");
+        } /* ceoTransitionHasEnded   */
+    }
+    /* End CeoHTML_Manager */	
 
     /* Begin TableManager  */
     let TableManager = {
         //Properties
 
         //Methods
-        generateTable: function(tableType, tableId){
+        generateTable: function(lsType){
         /* alert("lsType = " + lsType); */
             EduHTML_Manager.load("html/Education/Resources/Data_Sheets/LS107/LS107_Data_Sheet.html", "edu-article");
         },
@@ -658,9 +754,8 @@ let PracticeTruthTableManager = {
 
     /* End TableManager  */
 
-
-
     /* Begin CodesTabManager */
+
     let CodesTabManager = {
         //Properties
         introductionTabEle: {},
